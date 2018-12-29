c2839cd7610b6c09249fe3b21ce2fd6e74766c9f
PawarenessC <35727789+Xxawareness@users.noreply.github.com>
2018-12-30 08:11:43 +0900

    忘れてた

--- src/pawarenessc/coresys/Main.php
+++ src/pawarenessc/coresys/Main.php
@@ -72,7 +72,7 @@ use pocketmine\event\player\PlayerPreLoginEvent;
 
 use pocketmine\event\player\PlayerJoinEvent;
 
-
+use pocketmine\event\player\PlayerRespawnEvent;
 
 use pocketmine\event\player\PlayerQuitEvent;
 
